f9258901e929e6ada93f367f7a7d3150efcda951
Upendra Devisetty <upendra_35@iPlantAdmins-MacBook-Pro.local>
2016-06-29 22:19:09 -0700

    fix dispersion plot

--- summarizeResults.DESeq2.r
+++ summarizeResults.DESeq2.r
@@ -17,7 +17,7 @@ summarizeResults.DESeq2 <- function(out.DESeq2, group=target[,varInt], independe
   results <- out.DESeq2$results
   
   # diagnostic of the size factors
-  diagSizeFactorsPlots(dds=dds)
+  diagSizeFactorsPlots(dds=dds, output.file="diagSizeFactorsHist.png")
   
   # boxplots before and after normalisation
   source("/countsBoxplots.R")
