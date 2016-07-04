6c9db2d1a4cb2417428038ad31c120adf6f40d72
Upendra Devisetty <upendra_35@iPlantAdmins-MacBook-Pro.local>
2016-07-04 11:24:40 -0700

    modify the loadCountData function

--- run_deseq2_multi.r
+++ run_deseq2_multi.r
@@ -19,24 +19,24 @@ library(genefilter)
 
 args<-commandArgs(TRUE)
 
-options<-matrix(c('project',  'pn', 1,  "character",    	# project name
-                  'author', 'au', 1,  "character",      	# author of the statistical analysis/report  
-                  'Dir',  'r',  2,  "character",        	# path to the directory containing raw counts files
-		  'rawCounts', 'rc', 2, "character",		# path to the combined raw count file
-                  'OutDir',  'w',  1,  "character",     	# path to the output file 
-                  'target', 't',  1,  "character",      	# path to the design/target file
-                  'features', 'fe', 2,  "character",    	# names of the features to be removed (specific HTSeq-count information and rRNA for example)
-                  'varInt', 'v',  2,  "character",      	# factor of interest
-                  'condRef',  'c',  2,  "character",    	# reference biological condition
-                  'batch',  'b',  2,  "character",      	# blocking factor: NULL (default) or "batch" for example
-		  'locfunc', 'l', 2, "character", 		# "median" (default) or "shorth" to estimate the size factors
-		  'fitType', 'f',	2, "character", 	# mean-variance relationship: "parametric" (default) or "local"
-		  'cooksCutoff', 'cc',	2, "logical", 		# outliers detection threshold (TRUE to let DESeq2 choosing it or FALSE to disable the outliers detection)
-		  'independentFiltering', 'if', 2, "logical", 	# TRUE or FALSE to perform the independent filtering or not
-		  'typeTrans', 'tt', 2, "character",		# transformation method for PCA/clustering with DESeq2: "VST" or "rlog"
-                  'alpha',  'a',  2,  "double",         	# threshold of statistical significance
-                  'pAdjust',  'p',  2,  "character",    	# p-value adjustment method: "BH" (default) or "BY"
-                  'colors', 'co', 2,  "character",      	# vector of colors of each biological condition on the plots
+options<-matrix(c('project',  'pn', 1,  "character",      # project name
+                  'author', 'au', 1,  "character",        # author of the statistical analysis/report  
+                  'Dir',  'r',  2,  "character",          # path to the directory containing raw counts files
+                  'rawCounts', 'rc', 2, "character",    # path to the combined raw count file
+                  'OutDir',  'w',  1,  "character",       # path to the output file 
+                  'target', 't',  1,  "character",        # path to the design/target file
+                  'features', 'fe', 2,  "character",      # names of the features to be removed (specific HTSeq-count information and rRNA for example)
+                  'varInt', 'v',  2,  "character",        # factor of interest
+                  'condRef',  'c',  2,  "character",      # reference biological condition
+                  'batch',  'b',  2,  "character",        # blocking factor: NULL (default) or "batch" for example
+                  'locfunc', 'l', 2, "character",     # "median" (default) or "shorth" to estimate the size factors
+                  'fitType', 'f', 2, "character",   # mean-variance relationship: "parametric" (default) or "local"
+                  'cooksCutoff', 'cc',  2, "logical",     # outliers detection threshold (TRUE to let DESeq2 choosing it or FALSE to disable the outliers detection)
+                  'independentFiltering', 'if', 2, "logical",   # TRUE or FALSE to perform the independent filtering or not
+                  'typeTrans', 'tt', 2, "character",    # transformation method for PCA/clustering with DESeq2: "VST" or "rlog"
+                  'alpha',  'a',  2,  "double",           # threshold of statistical significance
+                  'pAdjust',  'p',  2,  "character",      # p-value adjustment method: "BH" (default) or "BY"
+                  'colors', 'co', 2,  "character",        # vector of colors of each biological condition on the plots
                   'help',   'h',    0,      "logical"),
                             ncol=4,byrow=TRUE)
 
@@ -71,13 +71,12 @@ col <- ret.opts$colors
 target <- loadTargetFile(targetFile=targetFile, varInt=varInt, condRef=condRef, batch=batch)
 
 # loading counts
-if (rawDir) {
-counts <- loadCountData(target=target, rawDir=rawDir, header=FALSE, skip=0, featuresToRemove=featuresToRemove)
-} else
-{
-source("/loadCountData.R")
-counts <- loadCountData(target=target, counts.file=rawCounts, header=FALSE, skip=0, featuresToRemove=featuresToRemove)
-}
+if (!is.null(ret.opts$Dir)) {
+counts <- loadCountData(target=target, rawDir=rawDir, header=FALSE, skip=0, featuresToRemove=featuresToRemove)}
+
+if (!is.null(ret.opts$rawCounts)) {
+source("/Users/upendra_35/Documents/git.repos/deseq2_multi/loadCountData.R")
+counts <- loadCountData(target=target, rawCounts=rawCounts, header=FALSE, skip=0, featuresToRemove=featuresToRemove)}
 
 # description Plots
 source("/descriptionPlots.r")
